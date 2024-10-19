9c297c489aeef0f4d328f087e478b04dbe404a65
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-10-19 16:13:56 +0200

    Importata tabella da bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,4 +6,22 @@
 <h1>
     Laravel Model Controller
 </h1>
+
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">ID</th>
+        <th scope="col">Titolo</th>
+        <th scope="col"></th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+    </tbody>
+  </table>
 @endsection
