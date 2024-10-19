334a3b086772204737c2128d804c4c45831268f6
Gianmaria Fadda <faddagianmaria10@gmail.com>
2024-10-19 13:43:41 +0200

    fatto prova per vedere se server phpmysql sono collegati

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Movie;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,7 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    
+    $movies = Movie::all();
+
+    dd($movies);
+
     return view('welcome', [
     ]);
 });
